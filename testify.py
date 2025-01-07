c7f74b7f39fff876dfcb349f0dc975b3f953154e
sytandas <sytandas@gmail.com>
2025-01-07 12:04:07 +0530

    pos

---
+++ testify.py
@@ -0,0 +1,24 @@
+# neural net post mortem 
+import numpy as np 
+
+class NeuralNetwrok:
+    def __init__ (self, input_size, hidden_size, output_size):
+        
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    def forward(self, x):
+
+    
+    def backward(self, x):
+
+
+    def train(self, X, y, epochs, learning_rate):
+
+    
+
+if __name__ == "__main__":
+
